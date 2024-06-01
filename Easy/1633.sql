0d3ef09a48e71cf897e437d0fed1a384f55cbcfb
sednjs <swhong0929@naver.com>
2024-06-01 03:00:32 -0400

    Update 1633.sql
    selecting from two different tables without joining

--- Easy/1633.sql
+++ Easy/1633.sql
@@ -1,7 +1,8 @@
 # Write your MySQL query statement below
-SELECT 
-    contest_id,
-    ROUND(COUNT(DISTINCT user_id)/(SELECT COUNT(DISTINCT user_id) FROM Register)*100,2) AS percentage
-FROM Register
+# the percentage of the users registered in each contest rounded to two decimals.
+
+SELECT contest_id, 
+       ROUND(COUNT(DISTINCT user_id) / (SELECT COUNT(DISTINCT user_id) FROM Users) * 100,2) AS percentage
+FROM Register 
 GROUP BY contest_id
-ORDER BY percentage DESC,contest_id
+ORDER BY percentage DESC, contest_id
